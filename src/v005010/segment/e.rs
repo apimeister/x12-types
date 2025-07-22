3edb8337fff8a4faa4683c82d8f85e99aba6cb67
Jens Walter <jens@apimeister.com>
2025-07-22 14:50:07 +0200

    Update e.rs

--- src/v005010/segment/e.rs
+++ src/v005010/segment/e.rs
@@ -245,3 +245,67 @@ pub struct EMS {
     #[serde(rename = "25")]
     pub _25: Option<String>,
 }
+
+/// EFI - Electronic Format Identification
+#[derive(
+    Serialize,
+    Deserialize,
+    Clone,
+    Default,
+    Debug,
+    Validate,
+    PartialEq,
+    Eq,
+    DisplaySegment,
+    ParseSegment,
+)]
+pub struct EFI {
+    /// 783 - Security Level Code
+    #[serde(rename = "01")]
+    pub _01: Option<String>,
+    /// 66 - Free-Form Message Text
+    #[serde(rename = "02")]
+    pub _02: Option<String>,
+    /// 783 - Security Level Code
+    #[serde(rename = "03")]
+    pub _03: Option<String>,
+    /// 66 - Free-Form Message Text
+    #[serde(rename = "04")]
+    pub _04: Option<String>,
+    /// 799 - Version Identifier
+    #[serde(rename = "05")]
+    pub _05: Option<String>,
+    /// 798 - Program Identifier
+    #[serde(rename = "06")]
+    pub _06: Option<String>,
+    /// 799 - Version Identifier
+    #[serde(rename = "07")]
+    pub _07: Option<String>,
+    /// 798 - Program Identifier
+    #[serde(rename = "08")]
+    pub _08: Option<String>,
+    /// 799 - Version Identifier
+    #[serde(rename = "09")]
+    pub _09: Option<String>,
+    /// 798 - Program Identifier
+    #[serde(rename = "10")]
+    pub _10: Option<String>,
+    /// 757 - Binary Data
+    #[serde(rename = "11")]
+    pub _11: Option<String>,
+    /// 559 - File Name
+    #[serde(rename = "12")]
+    pub _12: Option<String>,
+    /// 560 - Block Type
+    #[serde(rename = "13")]
+    pub _13: Option<String>,
+    /// 631 - Record Length
+    #[serde(rename = "14")]
+    pub _14: Option<String>,
+    /// 632 - Block Length
+    #[serde(rename = "15")]
+    pub _15: Option<String>,
+    /// 799 - Version Identifier
+    #[serde(rename = "16")]
+    pub _16: Option<String>,
+}
